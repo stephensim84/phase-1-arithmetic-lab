792c2ebadff0c77528acf30f4f5c027d6a9b0836
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-13 21:07:19 -0700

    commit tree a86d3f14d9a0170881f1e6db799497b158a1a9a8

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+//function math (){ 
+    
 
-// Write your code here
+
+    const num1 = 31;
+
+    const num2 = 2;
+    const multiply = num1 * num2;
+    //return multiply;
+    //
+    //}
+    
+    let random =  Math.random(); // generates a random #
+    //
+    //random = random > 0 ? random : '';
+    
+    
+    //console.log("XXXXXXXXXXXXX", random);
+    
+    //if(random > 0) {
+       // random = random;
+    //}Num
+    random = Number(Math.floor(Math.random() * 10) + 1);
+    
+    
+    const num3 = 14; 
+    const num4 = 10;
+    const mod = num3 % num4
+    
+    
+    const max = Math.max(15,16,17,18,19,20);
